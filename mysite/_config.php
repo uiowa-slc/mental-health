69aafbda447fb6ae1dfab40a567ec2f25bbcb650
Dustin Quam <quamsta@gmail.com>
2020-10-21 15:51:38 -0500

    updated editor config

--- mysite/_config.php
+++ mysite/_config.php
@@ -1,20 +1,11 @@
 <?php
 
-use SilverStripe\i18n\i18n;
-use SilverStripe\ORM\Search\FulltextSearchable;
-use SilverStripe\CMS\Model\SiteTree;
-use SilverStripe\View\Requirements;
-use SilverStripe\Security\MemberAuthenticator\MemberAuthenticator;
-use SilverStripe\Security\Authenticator;
-use SilverStripe\Control\Director;
-use SilverStripe\ActiveDirectory\Authenticators\SAMLAuthenticator;
 use SilverStripe\Forms\HTMLEditor\HtmlEditorConfig;
-use SilverStripe\Forms\HTMLEditor\TinyMCEConfig;
+use SilverStripe\i18n\i18n;
 
 // Set the site locale
 i18n::set_locale('en_US');
 // FulltextSearchable::enable();
-
 
 HtmlEditorConfig::get('cms')->addButtonsToLine(2, 'sup', 'separator');
 HtmlEditorConfig::get('cms')->removeButtons('pasteword');
